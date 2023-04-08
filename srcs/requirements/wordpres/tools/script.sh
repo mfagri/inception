70fa051516f0a24ce81655e62d543c4336ac1c19
Marouane Fagri <mfagri@e2r7p3.1337.ma>
2023-04-08 02:40:51 +0000

    fix redis

--- srcs/requirements/wordpres/tools/script.sh
+++ srcs/requirements/wordpres/tools/script.sh
@@ -15,6 +15,7 @@ wp config set WP_CACHE_KEY_SALT 'localhost'  --path="/var/www/html" --allow-root
 wp config set WP_REDIS_HOST 'redis'  --path="/var/www/html" --allow-root
 wp config set WP_REDIS_PORT '6379'  --path="/var/www/html" --allow-root
 wp config set WP_REDIS_SCHEME 'tcp'  --path="/var/www/html" --allow-root
+wp config set WP_REDIS_TIMEOUT 60 --path="/var/www/html" --allow-root
 wp config set WP_CACHE true --path="/var/www/html" --allow-root
 #install pluging and activate it and enable redis
 wp plugin install redis-cache --activate --path="/var/www/html" --allow-root
@@ -24,4 +25,12 @@ sleep 5
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 mkdir run/php
 echo "creat run/php floder"
+
+
+wp config get WP_REDIS_HOST --path="/var/www/html" --allow-root
+wp config get WP_REDIS_PORT --path="/var/www/html" --allow-root
+wp config get WP_REDIS_SCHEME --path="/var/www/html" --allow-root
+
+
+
 echo "star wordpress" && /usr/sbin/php-fpm7.3 -F 
